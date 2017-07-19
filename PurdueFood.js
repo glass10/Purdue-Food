e5a74cffdfc95feabaf6e7d368d27151afe90a2d
Cameron Glass <glass10@purdue.edu>
2017-07-19 15:57:33 -0400

    Store values from user and check for undefined and incorrect info

--- PurdueFood.js
+++ PurdueFood.js
@@ -16,7 +16,7 @@ exports.handler = (event, context) => {
         console.log(`LAUNCH REQUEST`)
         context.succeed(
           generateResponse(
-            buildSpeechletResponse("", false),
+            buildSpeechletResponse("Welcome to Purdue Food. How may I help you?", false),
             {}
           )
         )
@@ -28,6 +28,76 @@ exports.handler = (event, context) => {
 
         switch(event.request.intent.name) {
           case "mealReq":
+            var court = event.request.intent.slots.Court.value;
+            var date = event.request.intent.slots.Date.value;
+            var meal = event.request.intent.slots.Meal.value;
+
+
+            //Today's Information
+            var today = new Date();
+            var dd = today.getDate();
+            var mm = today.getMonth()+1; //January is 0!
+            var yyyy = today.getFullYear();
+            var hours = today.getHours() - 4; //Eastern Standard Time
+            var mins = today.getMinutes();
+            var secs = today.getSeconds();
+            console.log(hours);
+            if(dd<10) {
+                dd='0'+dd
+            } 
+            if(mm<10) {
+                mm='0'+mm
+            } 
+            today = mm+'-'+dd+'-'+yyyy;
+
+            //Handling empty values
+            if(court === "" || court === undefined){
+              court = 'Ford';
+            }
+            else if(court !== "Ford" && court !== "Wiley" && court !== "Earhart" && court !== "Hillenbrand" && court !== "Windsor"){
+              if(court[0] === 'f' || court[0] === 'F'){
+                court = "Ford";
+              }
+              else if(court[0] === 'e' || court[0] === 'E'){
+                court = "Earhart";
+              }
+              else if(court[0] === 'h' || court[0] === 'H'){
+                court = "Hillenbrand";
+              }
+              else if(court[2] === 'l' || court[2] === 'L'){
+                court = "Wiley";
+              }
+              else{
+                court = "Windsor";
+              }
+            }
+
+            
+            if(date === "" || date === undefined){
+              date = today;
+            }
+            if(meal === "" || meal === undefined){
+              if(hours < 11){
+                meal = "Breakfast"
+              }
+              if(hours >= 11 && hours < 5){
+                meal = "Lunch"
+              }
+              else{
+                meal = "Dinner"
+              }
+            }
+
+            console.log("Court: " + court);
+            console.log("Meal: " + meal);
+            console.log("Date: " + date);
+            context.succeed(
+                generateResponse(
+                    buildSpeechletResponse('The values I heard were: ' + court + ", " + meal + ", and " + date, true),
+                    {}
+                )
+            )
+
             break;
 
             case "AMAZON.HelpIntent":
