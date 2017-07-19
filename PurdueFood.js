93b4c59c682c9e749c7c71e587774f3b0e2d92e8
Cameron Glass <glass10@purdue.edu>
2017-07-19 13:39:17 -0400

    Add All Files

---
+++ PurdueFood.js
@@ -0,0 +1,106 @@
+var https = require('https')
+
+exports.handler = (event, context) => {
+
+  try {
+
+    if (event.session.new) {
+      // New Session
+      console.log("NEW SESSION")
+    }
+
+    switch (event.request.type) {
+
+      case "LaunchRequest":
+        // Launch Request
+        console.log(`LAUNCH REQUEST`)
+        context.succeed(
+          generateResponse(
+            buildSpeechletResponse("", false),
+            {}
+          )
+        )
+        break;
+
+      case "IntentRequest":
+        // Intent Request
+        console.log(`INTENT REQUEST`)
+
+        switch(event.request.intent.name) {
+          case "mealReq":
+            break;
+
+            case "AMAZON.HelpIntent":
+                console.log('HELP REQUEST');
+                context.succeed(
+                    generateResponse(
+                        buildSpeechletResponse('', false),
+                        {}
+                    )
+                )
+            break;
+            
+            case "AMAZON.CancelIntent":
+                console.log('HELP REQUEST');
+                context.succeed(
+                    generateResponse(
+                        buildSpeechletResponse('', true),
+                        {}
+                    )
+                )
+            break;
+            
+            case "AMAZON.StopIntent":
+                console.log('HELP REQUEST');
+                context.succeed(
+                    generateResponse(
+                        buildSpeechletResponse('', true),
+                        {}
+                    )
+                )
+            break;
+
+          default:
+            throw "Invalid intent"
+        }
+
+        break;
+
+      case "SessionEndedRequest":
+        // Session Ended Request
+        console.log(`SESSION ENDED REQUEST`)
+        break;
+
+      default:
+        context.fail(`INVALID REQUEST TYPE: ${event.request.type}`)
+
+    }
+
+  } catch(error) { context.fail(`Exception: ${error}`) }
+
+}
+
+
+
+// Helpers
+buildSpeechletResponse = (outputText, shouldEndSession) => {
+
+  return {
+    outputSpeech: {
+      type: "PlainText",
+      text: outputText
+    },
+    shouldEndSession: shouldEndSession
+  }
+
+}
+
+generateResponse = (speechletResponse, sessionAttributes) => {
+
+  return {
+    version: "1.0",
+    sessionAttributes: sessionAttributes,
+    response: speechletResponse
+  }
+
+}
